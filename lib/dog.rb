696114964e26b602b1fb4ecf4f39083fdea223c3
Alex Velasquez <jtinnesco@gmail.com>
2019-06-02 06:29:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,10 @@ class Dog
     @@all << self 
   end
   
-  def self.all 
-    @@all.each do {|dog| puts dog.name }
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
   end
   
 end
